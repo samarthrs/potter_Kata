03bdf997402e7824b066610443af297475b1a00a
unknown <Ssavanu@L-OFC758BP32.americas.gap.com>
2015-06-30 15:05:40 -0400

    Added Size 2 different book functionality and did more refactoring

--- src/main/java/harryPotter.java
+++ src/main/java/harryPotter.java
@@ -3,14 +3,13 @@ import java.util.Map;
 
 public class harryPotter {
 
-    public static final int EACH_BOOK_PRICE = 8;
-    public static double finalDiscountedPrice = 0.000;
-    public static Integer myBookCopies = 0;
+    private static final int EACH_BOOK_PRICE = 8;
+    private static final double DISCOUNTED_PRICE_FOR_TWO_DIFFERENT_BOOKS = 7.60;
+    private static double finalDiscountedPrice = 0.000;
+    private static Integer myBookCopies = 0;
 
-    public static double calculateBestPrice(Integer... books){
-
-        Map<Integer, Integer> myMap = new LinkedHashMap<Integer, Integer>();
-
+    private static void mapMyBooksWithNumberOfCopies(Map<Integer, Integer> myMap, Integer books[])
+    {
         for (Integer book : books)
         {
             if (myMap.containsKey(book))
@@ -18,11 +17,27 @@ public class harryPotter {
             else
                 myMap.put(book, 1);
         }
+    }
+
+    public static double calculateBestPrice(Integer... books){
+
+        Map<Integer, Integer> myMap = new LinkedHashMap<Integer, Integer>();
+
+        mapMyBooksWithNumberOfCopies(myMap, books);
 
         for (Map.Entry<Integer, Integer> entry : myMap.entrySet())
         {
             myBookCopies = entry.getValue();
-            finalDiscountedPrice = (myBookCopies > 1) ? EACH_BOOK_PRICE * myBookCopies : EACH_BOOK_PRICE;
+
+            if (myMap.size() == 1)
+            {
+                finalDiscountedPrice = (myBookCopies > 1) ? EACH_BOOK_PRICE * myBookCopies : EACH_BOOK_PRICE;
+            }
+
+            if (myMap.size() == 2)
+            {
+                finalDiscountedPrice = DISCOUNTED_PRICE_FOR_TWO_DIFFERENT_BOOKS * myMap.size();
+            }
         }
 
         return finalDiscountedPrice;
--- src/test/java/harryPotterTests.java
+++ src/test/java/harryPotterTests.java
@@ -5,10 +5,11 @@ import static org.junit.Assert.*;
 public class harryPotterTests {
 
     public static final int EACH_BOOK_PRICE = 8;
+    private static final double DISCOUNTED_PRICE_FOR_TWO_DIFFERENT_BOOKS = 7.60;
     public static final double DELTA = 0.001;
 
     @Test
-    public void whenFirstBookCopyIsOneAndRestAreNone()
+    public void whenFirstBookCopyIsOneAndRestAreNoneThenNoDiscountIsApplied()
     {
         double expectedPrice = EACH_BOOK_PRICE;
         assertEquals(expectedPrice, harryPotter.calculateBestPrice(1), DELTA);
@@ -16,7 +17,7 @@ public class harryPotterTests {
     }
 
     @Test
-    public void whenFirstBookCopiesAreTwoAndRestAreNone()
+    public void whenFirstBookCopiesAreTwoAndRestAreNoneThenNoDiscountIsApplied()
     {
         double expectedPrice = EACH_BOOK_PRICE * 2;
         assertEquals(expectedPrice, harryPotter.calculateBestPrice(1, 1), DELTA);
@@ -24,32 +25,47 @@ public class harryPotterTests {
 
 
     @Test
-    public void whenFirstBookCopiesAreThreeAndRestAreNone()
+    public void whenFirstBookCopiesAreThreeAndRestAreNoneThenNoDiscountIsApplied()
     {
         double expectedPrice = EACH_BOOK_PRICE * 3;
         assertEquals(expectedPrice, harryPotter.calculateBestPrice(1, 1, 1), DELTA);
     }
 
-//    @Test
-//    public void whenFirstAndSecondBookCopiesAreOneAndRestAreNone()
-//    {
-//        double expectedPrice = EACH_BOOK_PRICE * 2 * 0.95;
-//        assertEquals(expectedPrice, harryPotter.calculateBestPrice(1, 1), DELTA);
-//    }
-//
-//    @Test
-//    public void whenFirstAndSecondBookCopiesAreTwoAndRestAreNone()
-//    {
-//        double expectedPrice = EACH_BOOK_PRICE * 4 * 0.95;
-//        assertEquals(expectedPrice, harryPotter.calculateBestPrice(2, 2), DELTA);
-//    }
+    @Test
+    public void whenFirstAndSecondBookCopiesAreOneAndRestAreNoneThenFivePercentDiscountForBoth()
+    {
+        double expectedPrice = DISCOUNTED_PRICE_FOR_TWO_DIFFERENT_BOOKS * 2;
+        assertEquals(expectedPrice, harryPotter.calculateBestPrice(0, 1), DELTA);
+    }
 
-//    @Test
-//    public void whenFirstIsThreeAndSecondIsTwoAndRestAreNone()
-//    {
-//        double expectedPrice = EACH_BOOK_PRICE * 4 * 0.95 + EACH_BOOK_PRICE * 1;
-//        assertEquals(expectedPrice, harryPotter.calculateBestPrice(3, 2), DELTA);
-//    }
+    @Test
+    public void whenSecondAndThirdBookCopiesAreOneAndRestAreNoneThenFivePercentDiscountForBoth()
+    {
+        double expectedPrice = DISCOUNTED_PRICE_FOR_TWO_DIFFERENT_BOOKS * 2;
+        assertEquals(expectedPrice, harryPotter.calculateBestPrice(1, 2), DELTA);
+    }
+
+    @Test
+    public void whenThirdAndFourthBookCopiesAreOneAndRestAreNoneThenFivePercentDiscountForBoth()
+    {
+        double expectedPrice = DISCOUNTED_PRICE_FOR_TWO_DIFFERENT_BOOKS * 2;
+        assertEquals(expectedPrice, harryPotter.calculateBestPrice(2, 3), DELTA);
+    }
+
+    @Test
+    public void whenFourthAndFifthBookCopiesAreOneAndRestAreNoneThenFivePercentDiscountForBoth()
+    {
+        double expectedPrice = DISCOUNTED_PRICE_FOR_TWO_DIFFERENT_BOOKS * 2;
+        assertEquals(expectedPrice, harryPotter.calculateBestPrice(3, 4), DELTA);
+    }
+
+    @Test
+    public void whenFifthAndFirstBookCopiesAreOneAndRestAreNoneThenFivePercentDiscountForBoth()
+    {
+        double expectedPrice = DISCOUNTED_PRICE_FOR_TWO_DIFFERENT_BOOKS * 2;
+        assertEquals(expectedPrice, harryPotter.calculateBestPrice(4, 1), DELTA);
+    }
+
 
 
 }
